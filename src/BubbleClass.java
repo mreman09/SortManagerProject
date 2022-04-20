ded905fd6609e7e33c12c7a85490dc0527d333af
Muhammad Teladia <P2577087@my365.dmu.ac.uk>
2022-04-20 15:51:01 +0100

    Added bubble sort method

--- src/BubbleClass.java
+++ src/BubbleClass.java
@@ -1,3 +1,15 @@
 public class BubbleClass {
-
+        public int[] bubbleSort(int[] list){
+            int i, j, temp = 0;
+            for (i=0; i<list.length-1; i++){
+                for (j=0; j< list.length-1-i; j++){
+                    if(list[j]> list[j+1]){
+                        temp = list[j+1];
+                        list[j]= list[j+1];
+                        list[j+1]= temp;
+                    }
+                }
+            }
+            return list;
+        }
 }
