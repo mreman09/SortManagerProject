cdaa724fc7515b071e424f6da42917965c74e8cd
Muhammad Teladia <P2577087@my365.dmu.ac.uk>
2022-04-20 17:07:41 +0100

    Fixed bug in BubbleClass

--- src/BubbleClass.java
+++ src/BubbleClass.java
@@ -1,15 +1,15 @@
 public class BubbleClass {
-        public int[] bubbleSort(int[] list){
-            int i, j, temp = 0;
-            for (i=0; i<list.length-1; i++){
-                for (j=0; j< list.length-1-i; j++){
-                    if(list[j]> list[j+1]){
-                        temp = list[j+1];
-                        list[j]= list[j+1];
-                        list[j+1]= temp;
+        public void bubbleSort(int[] array){
+
+            for (int i=0; i<array.length - 1; i++){
+                for (int j=0; j< array.length- 1 - i; j++){
+                    if(array[j]> array[j+1]){
+                        int temp = array[j];
+                        array[j]= array[j+1];
+                        array[j+1]= temp;
                     }
                 }
             }
-            return list;
+
         }
 }
